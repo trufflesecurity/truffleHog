29f2ea41f951aa10bbde05f8c875036cd660b312
Sahil Silare <sahilsilare@gmail.com>
2024-11-09 23:44:52 +0530

    feat: introduced a new detector for  apis
    Signed-off-by: Sahil Silare <sahilsilare@gmail.com>

--- pkg/detectors/brandfetch/v1/brandfetch.go
+++ pkg/detectors/brandfetch/v1/brandfetch.go
@@ -16,12 +16,11 @@ type Scanner struct{}
 
 // Ensure the Scanner satisfies the interface at compile time.
 var _ detectors.Detector = (*Scanner)(nil)
-
 var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"brandfetch"}) + `\b([a-zA-Z0-9=]{44})`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"brandfetch"}) + `\b([0-9A-Za-z]{40})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
@@ -33,28 +32,27 @@ func (s Scanner) Keywords() []string {
 // FromData will find and optionally verify Brandfetch secrets in a given set of bytes.
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
 	dataStr := string(data)
-
 	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
-
 	for _, match := range matches {
 		if len(match) != 2 {
 			continue
 		}
 		resMatch := strings.TrimSpace(match[1])
-
 		s1 := detectors.Result{
 			DetectorType: detectorspb.DetectorType_Brandfetch,
 			Raw:          []byte(resMatch),
 		}
 
 		if verify {
-			// API upgraded to v2 from v1, new API doc:  https://docs.brandfetch.com/reference/brand-api
-			req, err := http.NewRequestWithContext(ctx, "GET", "https://api.brandfetch.io/v2/brands/google.com", nil)
+			payload := strings.NewReader(`{
+ 				"domain": "www.example.com"
+ 				}`)
+			req, err := http.NewRequestWithContext(ctx, "POST", "https://api.brandfetch.io/v1/color", payload)
 			if err != nil {
 				continue
 			}
 			req.Header.Add("Content-Type", "application/json")
-			req.Header.Add("Authorization", "Bearer "+resMatch)
+			req.Header.Add("x-api-key", resMatch)
 			res, err := client.Do(req)
 			if err == nil {
 				defer res.Body.Close()
@@ -63,17 +61,13 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 				}
 			}
 		}
-
 		results = append(results, s1)
 	}
-
 	return results, nil
 }
-
 func (s Scanner) Type() detectorspb.DetectorType {
 	return detectorspb.DetectorType_Brandfetch
 }
-
 func (s Scanner) Description() string {
 	return "Brandfetch is a service that provides brand data, including logos, colors, fonts, and more. Brandfetch API keys can be used to access this data."
 }
